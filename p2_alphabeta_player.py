274c1bb578368d63634e0b31e1e304d3f7e05e7f
Kirill Rudenko <kirillrudenko@MacBook-Pro-Kirill.local>
2015-05-07 06:41:15 +0300

    1-3

--- p2_alphabeta_player.py
+++ p2_alphabeta_player.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Sivasubramanian Chandrasegarampillai, Walter Curnow'
 __email__ = 'rchandra@uci.edu,wcurnow@uci.edu'
- 
-from assignment2 import Player, State, Action
-from operator import itemgetter
 
-class MinimaxPlayer(Player):
+from assignment2 import Player, State, Action
+
+class AlphaBetaPlayer(Player):
 	def minimax_helper(self, state):
 		#gets legal moves
- 		moves = state.actions()
-
+		moves = state.actions()
+		alpha = 2
+		beta = -2
 		solutions = []
 		visited = {}
 		for x in moves:
-			solutions.insert(0, (x, self.min_turn(state.result(x), visited)))
-		
+			solutions.insert(0, (x, self.min_turn(state.result(x), visited, alpha, beta)))
 		#sort by utility attribute, the second item in the tuple
 		solutions.sort(key = lambda a: a[1])
 		
@@ -26,8 +25,8 @@ class MinimaxPlayer(Player):
 		return self.minimax_helper(state)
 
 
- 	
-	def max_turn(self, state, visited):
+	
+	def max_turn(self, state, visited, alpha, beta):
 		if state.is_terminal():
 			visited[state] = state.utility(self)
 			return state.utility(self)
@@ -40,17 +39,18 @@ class MinimaxPlayer(Player):
 			if visited.get(state.result(x)) is not None:
 				solutions.insert(0, (x, visited[state.result(x)]))
 			else:
-				solutions.insert(0, (x, self.min_turn(state.result(x), visited)))
-        	
+				solutions.insert(0, (x, self.min_turn(state.result(x), visited, alpha, beta)))
         #sort by utility attribute, the second item in the tuple
-		solutions.sort(key=lambda a: a[1])
+			solutions.sort(key=lambda a: a[1])
         
-		Res = solutions[len(solutions)-1]
-		
+			Res = solutions[len(solutions)-1]
+			if Res[1] >= beta:
+				return Res[1]
+			if alpha < Res[1]:
+				alpha = Res[1]
 		return Res[1]
- 		
- 
-	def min_turn(self, state, visited):
+
+	def min_turn(self, state, visited, alpha, beta):
 		if state.is_terminal():
 			visited[state] = state.utility(self)
 			return state.utility(self)
@@ -63,11 +63,14 @@ class MinimaxPlayer(Player):
 			if visited.get(state.result(x)) is not None:
 				solutions.insert(0, (x, visited[state.result(x)]))
 			else:
-				solutions.insert(0, (x, self.max_turn(state.result(x), visited)))
-        	
+				solutions.insert(0, (x, self.max_turn(state.result(x), visited, alpha, beta)))
         #sort by utility attribute, the second item in the tuple
-		solutions.sort(key=lambda a: a[1])
+			solutions.sort(key=lambda a: a[1])
         
-		Res = solutions[0]
+			Res = solutions[0]
+			if Res[1] <= alpha:
+				return Res[1]
+			if beta > Res[1]:
+				beta = Res[1]
 		return Res[1]
 		
--- solutions/p3_evaluation_player.py
+++ solutions/p3_evaluation_player.py
@@ -50,6 +50,67 @@ class EvaluationPlayer(Player):
         Returns:
             the evaluation value (float), from 1.0 / state.K (worst) to 1.0 (win).
         """
-
+        streak = []
+        #iterate through the board:
+        for i in range(0, state.M):
+            for j in range(0, state.N):
+                if (state.board[i][j] == color):
+                    streak.insert(0, self.streak(state.board, i, j))
         # TODO implement this
-        return 0.0
+        streak.sort();
+        return float(streak[len(streak)-1])/state.K
+
+    def streak(self, board, i, j):
+        toreturn = 1
+        longest = 1
+        x = i
+        y = j
+        #in line
+        for y in range(j+1, len(board[0]) - 1):
+            if board[x][y] == board[i][j]:
+                longest = longest + 1
+            else:   
+                break
+        toreturn = longest
+        #in column
+        longest = 1
+        y = j
+        for x in range(i+1, len(board) - 1):
+            if board[x][y] == board[i][j]:
+                longest = longest + 1
+            else:
+                break
+        if (longest > toreturn):
+            toreturn = longest
+        longest = 1
+        x = i+1
+        y = j+1
+
+        while (x < len(board)) and (y < len(board[0])):
+            if board[x][y] == board[i][j]:
+                longest = longest + 1
+                x = x + 1
+                y = y + 1
+            else:
+                break
+        if (longest > toreturn):
+            toreturn = longest
+
+        longest = 1
+        x = i+1
+        y = j-1
+        while (x < len(board)) and (y >= 0):
+            if board[x][y] == board[i][j]:
+                longest = longest + 1
+                x = x + 1
+                y = y - 1
+            else:
+                break
+        if (longest > toreturn):
+            toreturn = longest
+        return toreturn
+
+
+
+
+
