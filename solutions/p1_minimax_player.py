674dbff34758c116aefa958ec7dca3904d4253cf
Andrew Dang <a7dang@gmail.com>
2015-05-07 19:13:42 -0700

    Comments for p1 p2

--- solutions/p1_minimax_player.py
+++ solutions/p1_minimax_player.py
@@ -5,16 +5,22 @@ from assignment2 import Player, State, Action
 
 class MinimaxPlayer(Player):
 	
+	# This is a recursive helper method for move() to determine the utility for the node in pov of max
 	def max_turn(self, state):
+
+		# Base case: Did we reach an end state?
 		if(state.is_terminal()):
+			# Determines and returns utility of that node
 			return state.utility(self)
 
-		# Grabs possible actions
+		# Grabs possible actions to perform on this state
 		possible_actions = state.actions()
 		best_action = possible_actions[0]
 		# Lowest possible utility first	
 		best_utility = -2
 
+		# Loop through each action, determine resulting state, determine utility,
+		# is it the best utility in pov of max?
 		for action in possible_actions:
 			new_state = state.result(action)
 			current_utility = self.min_turn(new_state)
@@ -25,19 +31,25 @@ class MinimaxPlayer(Player):
 
 		return best_utility
 
-
+	# This is a recursive helper method for move() to determine the utility for the node in pov of min
 	def min_turn(self, state):
+		# Base case: Did we reach an end state?
 		if(state.is_terminal()):
+			# Determines and returns utility of that node
 			return state.utility(self)
-		# Grabs possible actions
+			
+		# Grabs possible actions to perform on this state	
 		possible_actions = state.actions()
 		best_action = possible_actions[0]
 		# Highest possible utility first	
 		best_utility = 2
 
+		# Loop through each action, determine resulting state, determine utility,
+		# is it the best utility in pov of min?
 		for action in possible_actions:
 			new_state = state.result(action)
 			current_utility = self.max_turn(new_state)
+
 			if(current_utility < best_utility):
 				best_utility = current_utility
 				best_action = action
@@ -58,16 +70,19 @@ class MinimaxPlayer(Player):
 		# depth is used to distinguish between really good moves and just good moves
 
 		# We start with calculating min because we are in perspective of max
-		# Grabs possible actions
+		# Grabs possible actions to perform on this state
 		possible_actions = state.actions()
 		best_action = possible_actions[0]
 		# Lowest possible utility first	
 		best_utility = -2
 
+		# Loop through each action, determine resulting state, determine utility,
+		# is it the best utility in pov of max?
 		for action in possible_actions:
 			new_state = state.result(action)
 			current_utility = self.min_turn(new_state)
 
+			# We are using best utility to determine the best action.
 			if(current_utility > best_utility):
 				best_utility = current_utility
 				best_action = action
--- solutions/p2_alphabeta_player.py
+++ solutions/p2_alphabeta_player.py
@@ -4,17 +4,22 @@ __author__ = 'krudenko@ucsd.edu, kdduong@ucsd.edu, a9dang@ucsd.edu'
 from assignment2 import Player, State, Action
 
 class MinimaxPlayer(Player):
-	
+# This is a recursive helper method for move() to determine the utility for the node in pov of max
 	def max_turn(self, state, alpha, beta):
+
+		# Base case: Did we reach an end state?	
 		if(state.is_terminal()):
 			return state.utility(self)
 
-		# Grabs possible actions
+		# Grabs possible actions to perform on this state
 		possible_actions = state.actions()
 		best_action = possible_actions[0]
 		# Lowest possible utility first	
 		best_utility = -2
 
+		# Loop through each action, determine resulting state, determine utility,
+		# is it the best utility in pov of max?
+		# includes alpha beta pruning
 		for action in possible_actions:
 			new_state = state.result(action)
 			current_utility = self.min_turn(new_state, alpha, beta)
@@ -29,16 +34,22 @@ class MinimaxPlayer(Player):
 
 		return best_utility
 
-
+# This is a recursive helper method for move() to determine the utility for the node in pov of min
 	def min_turn(self, state, alpha, beta):
+
+		# Base case: Did we reach an end state?
 		if(state.is_terminal()):
 			return state.utility(self)
-		# Grabs possible actions
+
+		# Grabs possible actions to perform on this state
 		possible_actions = state.actions()
 		best_action = possible_actions[0]
 		# Highest possible utility first	
 		best_utility = 2
 
+		# Loop through each action, determine resulting state, determine utility,
+		# is it the best utility in pov of min?
+		# includes alpha beta pruning
 		for action in possible_actions:
 			new_state = state.result(action)
 			current_utility = self.max_turn(new_state, alpha, beta)
@@ -66,18 +77,24 @@ class MinimaxPlayer(Player):
 		# depth is used to distinguish between really good moves and just good moves
 
 		# We start with calculating min because we are in perspective of max
-		# Grabs possible actions
+		# Grabs possible actions to perform on this state
 		possible_actions = state.actions()
 		best_action = possible_actions[0]
+
 		# Lowest possible utility first	
 		best_utility = -2
+		# Lowest Alpha value first
 		alpha = -100
+		# Highest beta value first
 		beta = 100
 
+		# Loop through each action, determine resulting state, determine utility,
+		# is it the best utility in pov of max?
 		for action in possible_actions:
 			new_state = state.result(action)
 			current_utility = self.min_turn(new_state, alpha, beta)
 
+			# We are using best utility to determine the best action.
 			if(current_utility > best_utility):
 				best_utility = current_utility
 				best_action = action
